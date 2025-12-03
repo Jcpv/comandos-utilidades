416a107302c0e86dfb64e0b8d2e7667591337c69
José Cándido <josecandidop@hotmail.com>
2025-12-03 15:41:27 -0600

    Empaquetar o comprimir archivo para pasarlo a la web

---
+++ pyf/tareas_comprimir.sh
@@ -0,0 +1,35 @@
+echo "=== Iniciando tareas ==="
+cd /var/www/html/pyf || exit
+
+echo "=== Ajustar permisos a carpeta CACHE ==="
+echo "Carpetas de la versión 3.3"
+sudo tar -czvf ojs-pyf-3.3.0-11.tar.gz ojs-3.3.0-11
+sudo tar -czvf ojs-pyf-3.3.0-12.tar.gz ojs-3.3.0-12
+sudo tar -czvf ojs-pyf-3.3.0-13.tar.gz ojs-3.3.0-13
+sudo tar -czvf ojs-pyf-3.3.0-14.tar.gz ojs-3.3.0-14
+sudo tar -czvf ojs-pyf-3.3.0-15.tar.gz ojs-3.3.0-15
+sudo tar -czvf ojs-pyf-3.3.0-16.tar.gz ojs-3.3.0-16
+sudo tar -czvf ojs-pyf-3.3.0-17.tar.gz ojs-3.3.0-17
+sudo tar -czvf ojs-pyf-3.3.0-18.tar.gz ojs-3.3.0-18
+sudo tar -czvf ojs-pyf-3.3.0-19.tar.gz ojs-3.3.0-19
+sudo tar -czvf ojs-pyf-3.3.0-20.tar.gz ojs-3.3.0-20
+sudo tar -czvf ojs-pyf-3.3.0-21.tar.gz ojs-3.3.0-21
+
+echo "Carpetas de la versión 3.4"
+sudo tar -czvf ojs-pyf-3.4.0-0.tar.gz ojs-3.4.0-0
+sudo tar -czvf ojs-pyf-3.4.0-1.tar.gz ojs-3.4.0-1
+sudo tar -czvf ojs-pyf-3.4.0-2.tar.gz ojs-3.4.0-2
+sudo tar -czvf ojs-pyf-3.4.0-3.tar.gz ojs-3.4.0-3
+sudo tar -czvf ojs-pyf-3.4.0-4.tar.gz ojs-3.4.0-4
+sudo tar -czvf ojs-pyf-3.4.0-5.tar.gz ojs-3.4.0-5
+sudo tar -czvf ojs-pyf-3.4.0-6.tar.gz ojs-3.4.0-6
+sudo tar -czvf ojs-pyf-3.4.0-7.tar.gz ojs-3.4.0-7
+sudo tar -czvf ojs-pyf-3.4.0-8.tar.gz ojs-3.4.0-8
+sudo tar -czvf ojs-pyf-3.4.0-9.tar.gz ojs-3.4.0-9
+
+
+echo ""
+echo "= = = = = = = = = = = = = ="
+echo "fin de compresión de ojs"
+echo "= = = = = = = = = = = = = ="
+echo ""
